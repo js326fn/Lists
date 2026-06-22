97edbc30bd242f4816509ef32ea9bffe8c2f7968
jsve <48867261+js326fn@users.noreply.github.com>
2026-06-22 18:50:05 +0200

    Update maltrail.py

--- Scripts/maltrail.py
+++ Scripts/maltrail.py
@@ -1,7 +1,15 @@
+from pathlib import Path
 import requests
 
 URL_ZDROJA = "https://raw.githubusercontent.com/stamparm/maltrail/ad92e6c51871616da58be6d2d5314203b506b8a5/trails/static/malware/fakeapp.txt"
-VYSTUPNY_SUBOR = "Lists/maltrail_urls.txt"
+
+root_projektu = Path(__file__).resolve().parent.parent
+priecinok_lists = root_projektu / "Lists"
+
+priecinok_lists.mkdir(parents=True, exist_ok=True)
+
+VYSTUPNY_SUBOR = priecinok_lists / "maltrail_urls.txt"
+# -----------------------------------------------------------
 
 def stiahni_a_rozparsuj():
     try:
@@ -29,11 +37,12 @@ def stiahni_a_rozparsuj():
         novy_riadok = f"{riadok},/,maltrail"
         osetrene_riadky.append(novy_riadok)
 
-
+    # Zápis do súboru na novej dynamickej ceste
     with open(VYSTUPNY_SUBOR, "w", encoding="utf-8") as f:
         f.write("\n".join(osetrene_riadky) + "\n")
         
-    print(f"Úspešne aktualizované! Uložených {len(osetrene_riadky)} riadkov.")
+    print(f"Úspešne aktualizované! Uložené v: {VYSTUPNY_SUBOR}")
+    print(f"Uložených {len(osetrene_riadky)} riadkov.")
 
 if __name__ == "__main__":
     stiahni_a_rozparsuj()
